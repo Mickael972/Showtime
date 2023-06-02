7662b76e3adf1f357d49fd0a557656360cd48880
Mickael972 <madallstar@gmail.com>
2023-06-02 15:29:16 +0200

    second commit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { MovieApiServiceService } from 'src/app/service/movie-api-service.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  constructor(private service:MovieApiServiceService) {}
+
+  bannerResult:any=[];
+
+  ngOnInit(): void {
+    this.bannerData();
+  }
+
+  //bannerdata
+  bannerData(){
+    this.service.bannerApiData().subscribe((result)=>{
+      console.log(result, 'bannerresult#');
+      this.bannerResult = result.results;
+    });
+  }
 
 }
