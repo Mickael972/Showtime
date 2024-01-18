b5331419f5a0998108bb70a8fae52e2f454bbf62
Mickael972 <madallstar@gmail.com>
2024-01-18 14:37:29 +0100

    commit all modifications

--- src/app/pages/movie-details/movie-details.component.ts
+++ src/app/pages/movie-details/movie-details.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieApiServiceService } from 'src/app/service/movie-api-service.service';
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.component.html',
   styleUrls: ['./movie-details.component.css']
 })
-export class MovieDetailsComponent {
+export class MovieDetailsComponent implements OnInit {
+
+  getMovieDetailsRes:any;
+  getMovieVideoRes:any;
+  getMovieCastRes:any;  
+
+  constructor(private service:MovieApiServiceService, private router:ActivatedRoute) { }
+
+  ngOnInit(): void {
+    let getParamId = this.router.snapshot.paramMap.get('id');
+    console.log(getParamId, 'getparamid#');
+
+    this.getMovieDetails(getParamId);
+    this.getVideo(getParamId);  
+    this.getCast(getParamId);
+  }
+
+
+
+  getMovieDetails(id:any){
+    this.service.getMovieDetails(id).subscribe((res)=>{
+      console.log(res, 'getmoviedetails#');
+      this.getMovieDetailsRes = res;
+    });
+  }
+
+
+  getVideo(id:any){
+    this.service.getMovieVideo(id).subscribe((res)=>{
+      console.log(res, 'getmovievideo#');
+      res.results.forEach((element:any) => {
+        if(element.type == "Trailer")
+        {
+          this.getMovieVideoRes = element.key;
+        }
+      });
+    });
+  }
+
+
+  getCast(id:any)
+  {
+    this.service.getMovieCast(id).subscribe((res)=>{
+      console.log(res, 'getmoviecast#');
+      this.getMovieCastRes = res.cast;
+    });
+  }
+
 
 }
--- src/app/service/movie-api-service.service.ts
+++ src/app/service/movie-api-service.service.ts
@@ -11,7 +11,7 @@ export class MovieApiServiceService {
   baseurl = "https://api.themoviedb.org/3";
   apikey = "08cc33bd5ae3a747598ce2ad84376e66";
 
-  //bannerapidate
+  //bannerapidata
   bannerApiData():Observable<any>
   {
     return this.http.get(`${this.baseurl}/trending/all/week?api_key=${this.apikey}`);
@@ -25,10 +25,31 @@ export class MovieApiServiceService {
   }
 
 
+  //searchmovie
   getSearchMovie(data:any):Observable<any>
   {
     console.log(data, 'movie');
     return this.http.get(`${this.baseurl}/search/movie?api_key=${this.apikey}&query=${data.movieName}`);
   }
 
+
+  //getmoviedetails
+  getMovieDetails(data:any):Observable<any> 
+  {
+    return this.http.get(`${this.baseurl}/movie/${data}?api_key=${this.apikey}`);
+  }
+
+
+  //getmovievideo
+  getMovieVideo(data:any):Observable<any>
+  {
+    return this.http.get(`${this.baseurl}/movie/${data}/videos?api_key=${this.apikey}`);
+  } 
+
+
+  //getmoviecast
+  getMovieCast(data:any):Observable<any>
+  {
+    return this.http.get(`${this.baseurl}/movie/${data}/credits?api_key=${this.apikey}`);
+  }
 }
